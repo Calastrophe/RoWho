6331004d94ff84aa856ee88144ce317e80b09665
Calastrophe <74928681+Calastrophe@users.noreply.github.com>
2021-01-13 21:26:41 -0600

    Add files via upload

---
+++ RoWho.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+username = input("Username of the individual: ")
+
+IDRequest = requests.get('https://api.roblox.com/users/get-by-username?username=' + username)
+RoID = ''.join(filter(lambda i: i.isdigit(), IDRequest.text))
+print("ID: " + str(RoID))
+Status = IDRequest.text.find('"IsOnline":True')
+
+if Status < 0:
+    print(username + " is currently offline.")
+if Status > 0:
+    print(username + " is currently online.")
+
+rap = 0
+hats = 0
+rinv = requests.get('https://inventory.roblox.com/v1/users/' + RoID + '/assets/collectibles?assetType=Hat&sortOrder=Asc&limit=100')
+invres = rinv.text
+cinvres = json.loads(invres)
+data = cinvres['data']
+for key in data:
+    hats += 1
+    rap += key['recentAveragePrice']
+print(username + "'s RAP is " + str(rap) + ' and has ' + str(hats) + ' limiteds')
+
+
