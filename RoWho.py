4b9c35f9fdeabb107ecb268929f3f9523a837c74
Calastrophe <74928681+Calastrophe@users.noreply.github.com>
2021-07-22 00:37:39 -0500

    Completely new rework of the script

--- RoWho.py
+++ RoWho.py
@@ -1,29 +1,29 @@
 import requests
 import json
 
-username = input("Username of the individual: ")
+# This is a useless tool, to be honest...
 
-IDRequest = requests.get('https://api.roblox.com/users/get-by-username?username=' + username)
-RoID = ''.join(filter(lambda i: i.isdigit(), IDRequest.text))
-print("ID: " + str(RoID))
-Status = IDRequest.text.find('"IsOnline":True')
-
-if Status < 0:
-    print(username + " is currently offline.")
-if Status > 0:
-    print(username + " is currently online.")
-
-rap = 0
-hats = 0
-rinv = requests.get('https://inventory.roblox.com/v1/users/' + RoID + '/assets/collectibles?assetType=Hat&sortOrder=Asc&limit=100')
-invres = rinv.text
-cinvres = json.loads(invres)
-data = cinvres['data']
-for key in data:
-    hats += 1
-    rap += key['recentAveragePrice']
-if hats == 100:
-    print("NOTICE: The RAP will be wrong, the player has over 100 hats and API does not support more requests than 100. He is most likely REALLY rich.")
-print(username + "'s RAP based on hats is " + str(rap) + ' and has ' + str(hats) + ' hats')
+def obtainInformation(user):
+    try:
+        ReqResponse = requests.get('https://api.roblox.com/users/get-by-username?username=' + user)
+        ResponseDict = json.loads(ReqResponse.text)
+        ID = ResponseDict["Id"]
+        print(user + "'s ID is: " + str(ID))
+        checkStatus(ResponseDict)
+    except Exception as err:
+        print(err)
 
 
+def checkStatus(ResponseDict):
+    try:
+        if ResponseDict["IsOnline"] == False:
+            print(user + ' is offline')
+        else:
+            print(user + ' is online')
+    except Exception as err:
+        print(err)
+
+
+if __name__ == "__main__":
+    user = input("Please input the username of target: ")
+    obtainInformation(user)
